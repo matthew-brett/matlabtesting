e645d4f0fdc01996e9a5b21c776545009a5e605d
Matthew Brett <matthew.brett@gmail.com>
2011-11-12 17:12:38 -0800

    BF - deal with NaNs in compared arrays
    Check that NaNs are in the same position in both arrays.

--- assert_equal.m
+++ assert_equal.m
@@ -2,6 +2,7 @@ function assert_equal(m1, m2, tol)
 % Raises error if m1, m2 not numerically close with threshold tol
 % FORMAT assert_equal(m1, m2, tol)
 % m1, m2 can be numeric or cell arrays
+% NaNs are allowed and have to be in the same location in each array
 
 if nargin < 3
   tol = 0;
@@ -29,8 +30,13 @@ for e = 1:numel(m1)
     end
     continue
   end
+  % NaNs can occur, but must be in the same place
+  nan1 = isnan(c1);
+  if any(nan1 ~= isnan(c2))
+      error('NaNs positions do not match in compared arrays');
+  end
   D = abs(c1 - c2);
-  if any(D(:)>tol)
+  if any(D(~nan1) > tol)
     error(['Matrices differ at tol ' num2str(tol)]);
   end
 end
